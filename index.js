fe594d089d3ff3800e48cb22e0b908b690bde389
vivpen29 <rickypenumaka@gmail.com>
2025-04-14 11:42:18 +0530

    https call to omdbapi.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fetchData = async searchTerm => {
-  const response = await axios.get('http://www.omdbapi.com/', {
+  const response = await axios.get('https://www.omdbapi.com/', {
     params: {
       apikey: 'd9835cc5',
       s: searchTerm
